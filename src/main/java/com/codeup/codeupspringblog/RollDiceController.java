109ab3c2850c2c4152cac13dde4f9523ac7d7ca8
Genesis Sarabia <genesissarabia14@gmail.com>
2023-05-18 10:48:43 -0500

    completed die roll exercise

---
+++ src/main/java/com/codeup/codeupspringblog/RollDiceController.java
@@ -0,0 +1,33 @@
+package com.codeup.codeupspringblog;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@Controller
+public class RollDiceController {
+
+
+    @GetMapping(path = "/roll-dice")
+    public String rollDice() {
+        return "guessNum";
+    }
+    @GetMapping(path="/roll-dice/{n}")
+    public String numGenerated(@PathVariable int n, Model model){
+        int randomNumber = (int)Math.floor(Math.random() * (6 - 1 + 1) + 1);
+        String guess = "";
+
+        if(randomNumber == n){
+           guess = "You got it";
+        } else {
+            guess = "Try again.";
+        }
+
+        model.addAttribute("userGuess", "Your guess is: " + n);
+        model.addAttribute("diceRoll", "The die rolled: " + randomNumber);
+        model.addAttribute("guess", guess);
+        return "guessNum";
+    }
+}
