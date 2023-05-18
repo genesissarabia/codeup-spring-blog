d8d3f55c95e78b16cda85ae639f1ef51a36b29b9
Genesis Sarabia <genesissarabia14@gmail.com>
2023-05-18 15:40:18 -0500

    completed index view, moving to repositories and JPA exercises

--- src/main/java/com/codeup/codeupspringblog/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/PostController.java
@@ -7,13 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String postsRoute() {
-        return "posts index page";
+    public String postsRoute(Model model) {
+        List<Post> posts = new ArrayList<>();
+        Post post1 = new Post(2L, "Roxy Pup", "Roxy is my baby chihuahua. She's an angel and I love her to the sky and back!");
+        Post post2 = new Post(3L, "Got a new lamp", "It's so pretty and it has floral print on the ceramic bottom. I got it at Hobby Lobby.");
+        Post post3 = new Post(4L, "Love 2000s Latino Pop", "I'm on a 00's Latino Pop kick with specifically OV7, La Oreja De Van Gogh and Shakira :D");
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post3);
+        model.addAttribute("posts", posts);
+        return "/posts/index";
     }
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
     public String onePostRoute(@PathVariable Long id, Model model) {
